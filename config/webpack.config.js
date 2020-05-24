95693e9dd8de5f5032d556e192ecb639de03669b
David <koroldavid08@gmail.com>
2020-05-24 19:15:17 +0300

    Config using less

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -49,6 +49,7 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
+const lessRegex = /\.less$/;
 
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
@@ -484,6 +485,23 @@ module.exports = function(webpackEnv) {
                 },
                 'sass-loader'
               ),
+            },
+             // Add LESS support
+            // using the extension .less
+            {
+              test: lessRegex,
+              use: getStyleLoaders(
+                {
+                  importLoaders: 4,
+                  sourceMap: isEnvProduction && shouldUseSourceMap,
+                },
+                'less-loader'
+              ),
+              // Don't consider CSS imports dead code even if the
+              // containing package claims to have no side effects.
+              // Remove this when webpack adds a warning or an error for this.
+              // See https://github.com/webpack/webpack/issues/6571
+              sideEffects: true,
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
